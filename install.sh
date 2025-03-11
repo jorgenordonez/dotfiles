0398b3ddba792bfcc0b2085f299a97f94bb3647e
Jorgen Ordonez <jorgenordonez@gmail.com>
2025-03-10 20:42:10 -0600

    added nvim lua files

--- install.sh
+++ install.sh
@@ -7,3 +7,11 @@ ln -s $DOTFILES/kitty $HOME/.config/kitty
 
 rm -rf $HOME/.tmux.conf
 ln -s $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
+
+mkdir -p $HOME/.local/bin
+
+rm -rf $HOME/.local/bin/t
+ln -s $DOTFILES/scripts/t $HOME/.local/bin/t
+
+rm -rf $HOME/.conig/nvim
+ln -s $DOTFILES/nvim $HOME/.config/nvim
